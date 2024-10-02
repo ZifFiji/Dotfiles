fa9ce5a085023ccb31b64a89e73efc869587199e
Arthur <arthur.guerinault@epitech.eu>
2024-10-02 14:06:19 +0200

    add: errors

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -119,6 +119,48 @@ require('lazy').setup({
     --       "nvim-telescope/telescope.nvim"
     --     }
     -- },
+
+    -- formatter
+    {
+      'stevearc/conform.nvim',
+    },
+    {
+      "folke/trouble.nvim",
+      opts = {}, -- for default options, refer to the configuration section for custom setup.
+      cmd = "Trouble",
+      keys = {
+        {
+          "<leader>xx",
+          "<cmd>Trouble diagnostics toggle<cr>",
+          desc = "Diagnostics (Trouble)",
+        },
+        {
+          "<leader>xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+          desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          "<leader>cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+          desc = "Symbols (Trouble)",
+        },
+        {
+          "<leader>cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+          desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          "<leader>xL",
+          "<cmd>Trouble loclist toggle<cr>",
+          desc = "Location List (Trouble)",
+        },
+        {
+          "<leader>xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+          desc = "Quickfix List (Trouble)",
+        },
+      },
+    },
     {
       "lewis6991/gitsigns.nvim",
       opts = {
@@ -175,11 +217,6 @@ require('lazy').setup({
         end,
       },
     },
-    -- formatter
-    {
-      'stevearc/conform.nvim',
-    },
-
     --clang
     "rhysd/vim-clang-format",
     })
---
+++ .config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,147 @@
+local lspconfig = require 'lspconfig'
+local configs = require 'lspconfig.configs'
+
+return {
+    -- the colorscheme should be available when starting Neovim
+    {
+      "folke/tokyonight.nvim",
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other start plugins
+      config = function()
+        -- load the colorscheme here
+        vim.cmd([[colorscheme tokyonight]]) end,
+    },
+  
+    -- I have a separate config.mappings file where I require which-key.
+    -- With lazy the plugin will be automatically loaded when it is required somewhere
+    { "folke/which-key.nvim", lazy = true },
+  
+    {
+      "nvim-neorg/neorg",
+      -- lazy-load on filetype
+      ft = "norg",
+      -- options for neorg. This will automatically call `require("neorg").setup(opts)`
+      opts = {
+        load = {
+          ["core.defaults"] = {},
+        },
+      },
+    },
+  
+    {
+      "dstein64/vim-startuptime",
+      -- lazy-load on a command
+      cmd = "StartupTime",
+      -- init is called during startup. Configuration for vim plugins typically should be set in an init function
+      init = function()
+        vim.g.startuptime_tries = 10
+      end,
+    },
+  
+    {
+      "hrsh7th/nvim-cmp",
+      -- load cmp on InsertEnter
+      event = "InsertEnter",
+      -- these dependencies will only be loaded when cmp loads
+      -- dependencies are always lazy-loaded unless specified otherwise
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+      },
+      config = function()
+        -- ...
+      end,
+    },
+  
+    -- if some code requires a module from an unloaded plugin, it will be automatically loaded.
+    -- So for api plugins like devicons, we can always set lazy=true
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+  
+    -- you can use the VeryLazy event for things that can
+    -- load later and are not important for the initial UI
+    { "stevearc/dressing.nvim", event = "VeryLazy" },
+  
+    {
+      "Wansmer/treesj",
+      keys = {
+        { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+      },
+      opts = { use_default_keymaps = false, max_join_length = 150 },
+    },
+  
+    {
+      "monaqa/dial.nvim",
+      -- lazy-load on keys
+      -- mode is `n` by default. For more advanced options, check the section on key mappings
+      keys = { "<C-a>", { "<C-x>", mode = "n" } },
+    },
+  
+    -- local plugins need to be explicitly configured with dir
+    { dir = "~/projects/secret.nvim" },
+  
+    -- you can use a custom url to fetch a plugin
+    { url = "git@github.com:folke/noice.nvim.git" },
+  
+    -- local plugins can also be configured with the dev option.
+    -- This will use {config.dev.path}/noice.nvim/ instead of fetching it from GitHub
+    -- With the dev option, you can easily switch between the local and installed version of a plugin
+    { "folke/noice.nvim", dev = true },
+    
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+          "MunifTanjim/nui.nvim",
+          -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
+    
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = { ensure_installed = { "cpp" } },
+    },
+    
+    {
+        "p00f/clangd_extensions.nvim",
+        lazy = true,
+        config = function() end,
+        opts = {
+          inlay_hints = {
+            inline = false,
+          },
+          ast = {
+            --These require codicons (https://github.com/microsoft/vscode-codicons)
+            role_icons = {
+              type = "",
+              declaration = "",
+              expression = "",
+              specifier = "",
+              statement = "",
+              ["template argument"] = "",
+            },
+            kind_icons = {
+              Compound = "",
+              Recovery = "",
+              TranslationUnit = "",
+              PackExpansion = "",
+              TemplateTypeParm = "",
+              TemplateTemplateParm = "",
+              TemplateParamObject = "",
+            },
+          },
+        },
+      },
+    {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    },
+
+      {"nvim-telescope/telescope.nvim", tag = "0.1.8",
+        dependencies = {"nvim-lua/plenary.nvim"}
+      },
+      
+      
+  }
